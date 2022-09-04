a19d6caef4941b158650d31fba7d23fcd71a1690
avemasha <masha.novosad@icloud.com>
2022-09-04 20:28:28 +0200

    markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,6 +9,8 @@ const galleryMarkup = createGalleryItem(galleryItems);
 galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
 galleryContainer.addEventListener("click", onGalleryClick);
 
+let gallery = new SimpleLightbox(".gallery a");
+
 function createGalleryItem(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -20,12 +22,14 @@ function createGalleryItem(galleryItems) {
     .join("");
 }
 
+ 
+
 function onGalleryClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-  let gallery = new SimpleLightbox(".gallery a");
+  // let gallery = new SimpleLightbox(".gallery a");
 
   gallery.on("show.simplelightbox", function () {
     gallery.defaultOptions.captionDelay = 250;
